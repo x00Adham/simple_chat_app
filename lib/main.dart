738c665f1ce05d9b06b44e98d4529fb26c3599bc
“Adham <adham.m.abdulah@gmail.com>
2025-07-31 18:10:36 +0300

    adding  assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:simple_chat_app/screens/login_screen.dart';
+import 'package:simple_chat_app/utils/themes/light_mode.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,8 +13,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      // theme: lightMode,
+      debugShowCheckedModeBanner: false,
+      home: WelcomePage(),
     );
   }
 }
-
