f289178d701abd05142d692717132eb778c030ec
“Adham <adham.m.abdulah@gmail.com>
2025-08-05 16:57:11 +0300

    Add ChatPage and integrate navigation from HomePage
    - Created a new ChatPage to display the chat interface with the selected user.- Updated HomePage to navigate to ChatPage when a user is tapped, passing the user's email as a parameter.- Improved the user list interaction by wrapping ListTile in a Container with margin for better spacing.

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:simple_chat_app/cubit/auth_cubit.dart';
+import 'package:simple_chat_app/screens/chat_page.dart';
 import 'package:simple_chat_app/services/auth_service.dart';
 import 'package:simple_chat_app/services/chat_Service.dart';
 import 'package:simple_chat_app/widgets/custom_drawer.dart';
@@ -34,13 +36,24 @@ class HomePage extends StatelessWidget {
                   itemBuilder: (context, index) {
                     final user = users[index];
                     final email = user['email']?.toString() ?? 'No email';
-                    return ListTile(
-                      leading: CircleAvatar(child: Icon(Icons.person)),
-
-                      title: 
-                        authService.getCurrentuser()?.email?.toString() == email
-                          ? Text("me")
-                          : Text(email),
+                    return Container(
+                      margin: EdgeInsets.only(top: 10),
+                      child: ListTile(
+                        onTap:
+                            () => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder:
+                                    (context) => ChatPage(receiverEmail: email),
+                              ),
+                            ),
+                        leading: CircleAvatar(child: Icon(Icons.person)),
+                        title:
+                            authService.getCurrentuser()?.email?.toString() ==
+                                    email
+                                ? Text("me")
+                                : Text(email),
+                      ),
                     );
                   },
                 );
