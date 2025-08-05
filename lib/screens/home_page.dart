11b286d01061a353e4a2bfb9a3e7c4aa1ed60fcb
“Adham <adham.m.abdulah@gmail.com>
2025-08-05 16:29:06 +0300

    Refactor HomePage to integrate ChatService and AuthService
    - Updated HomePage to utilize ChatService for real-time user data streaming.- Integrated AuthService to display the current user's email in the user list.- Simplified the app bar and removed the CustomDrawer implementation from HomePage.- Cleaned up the code for better readability and maintainability.

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
-import 'package:simple_chat_app/constants/colors.dart';
 import 'package:simple_chat_app/cubit/auth_cubit.dart';
-import 'package:simple_chat_app/gen/assets.gen.dart';
+import 'package:simple_chat_app/services/auth_service.dart';
+import 'package:simple_chat_app/services/chat_Service.dart';
+import 'package:simple_chat_app/widgets/custom_drawer.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+  final ChatService chatService = ChatService();
+  final AuthService authService = AuthService();
 
   @override
   Widget build(BuildContext context) {
@@ -14,89 +16,39 @@ class HomePage extends StatelessWidget {
       builder: (context, state) {
         return Scaffold(
           drawer: CustomDrawer(),
-          appBar: AppBar(
-            backgroundColor: MyColors.mainColor,
-            title: Text("Home"),
-            centerTitle: true,
+          appBar: AppBar(title: Text("Home"), centerTitle: true),
+          body: SafeArea(
+            child: StreamBuilder(
+              stream: chatService.getUserStream(),
+
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                }
+                if (!snapshot.hasData) {
+                  return Text('Loading...');
+                }
+                final users = snapshot.data ?? [];
+                return ListView.builder(
+                  itemCount: users.length,
+                  itemBuilder: (context, index) {
+                    final user = users[index];
+                    final email = user['email']?.toString() ?? 'No email';
+                    return ListTile(
+                      leading: CircleAvatar(child: Icon(Icons.person)),
+
+                      title: 
+                        authService.getCurrentuser()?.email?.toString() == email
+                          ? Text("me")
+                          : Text(email),
+                    );
+                  },
+                );
+              },
+            ),
           ),
         );
       },
-    );
-  }
-}
-
-class CustomDrawer extends StatelessWidget {
-  const CustomDrawer({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Drawer(
-      child: Column(
-        children: [
-          Assets.lib.assets.images.blueBack.image(),
-          // Divider(height: 5),
-          GestureDetector(
-            onTap: () {
-              context.go("/home");
-              Navigator.of(context).pop();
-            },
-            child: ListTile(
-              leading: Icon(Icons.home, color: Colors.grey.shade800),
-              title: Text(
-                "Home",
-                style: TextStyle(fontSize: 20, color: Colors.grey.shade800),
-              ),
-            ),
-          ),
-
-          //*Profile
-          GestureDetector(
-            onTap: () {
-              context.go("/profile");
-              Navigator.of(context).pop();
-            },
-            child: ListTile(
-              leading: Icon(Icons.person, color: Colors.grey.shade800),
-              title: Text(
-                "Profile",
-                style: TextStyle(fontSize: 20, color: Colors.grey.shade800),
-              ),
-            ),
-          ),
-
-          //*Settings
-          GestureDetector(
-            onTap: () {
-              context.go("/settings");
-              Navigator.of(context).pop();
-            },
-            child: ListTile(
-              leading: Icon(Icons.settings, color: Colors.grey.shade800),
-              title: Text(
-                "Settings",
-                style: TextStyle(fontSize: 20, color: Colors.grey.shade800),
-              ),
-            ),
-          ),
-          Spacer(),
-
-          //*logout
-          GestureDetector(
-            onTap: () {
-              context.read<AuthCubit>().logout();
-              context.go("/welcome");
-            },
-            child: ListTile(
-              leading: Icon(Icons.logout, color: Colors.grey.shade800),
-              title: Text(
-                "Logout",
-                style: TextStyle(fontSize: 20, color: Colors.grey.shade800),
-              ),
-            ),
-          ),
-          SizedBox(height: 40),
-        ],
-      ),
     );
   }
 }
--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -5,7 +5,7 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return   Scaffold(
+    return Scaffold(
     );
   }
 }
