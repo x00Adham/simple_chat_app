dffa68c0f9f5176be5d98d5bcd774f8fcc533126
“Adham <adham.m.abdulah@gmail.com>
2025-08-06 22:21:43 +0300

    Add message sending and retrieval functionality in ChatService
    - Implemented sendMessage method to handle sending messages between users, including user info and timestamp.- Added getMessages method to retrieve messages from the Firestore database for a specific chat.- Integrated FirebaseAuth for user authentication and Firestore for message storage.

--- lib/services/chat_service.dart
+++ lib/services/chat_service.dart
@@ -1,8 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:simple_chat_app/models/messages_model.dart';
 
 class ChatService {
   // Firestore database instance
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
   //get user stream
   Stream<List<Map<String, dynamic>>> getUserStream() {
@@ -12,5 +15,45 @@ class ChatService {
         return user;
       }).toList();
     });
+  }
+
+  //* send message
+  Future<void> sendMessage(String receiverId, message) async {
+    //* get user info
+    final String currentUserId = _auth.currentUser!.uid;
+    final String currentUserEmail = _auth.currentUser!.email!;
+    final Timestamp timestamp = Timestamp.now();
+
+    //* create  a new message
+    MessageModel newMessage = MessageModel(
+      message: message,
+      senderEmail: currentUserEmail,
+      senderId: currentUserId,
+      receiverId: receiverId,
+      timestamp: timestamp,
+    );
+
+    //* build the chat by sort users id
+
+    List<String> ids = [currentUserId, receiverId];
+    ids.sort();
+    String chatId = ids.join('_');
+
+    //* add the new message to database
+
+    await _firestore
+        .collection("chat_rooms")
+        .doc(chatId)
+        .collection("messages")
+        .add(newMessage.toMap());
+  }
+
+  //* get messages
+  Stream<QuerySnapshot> getMessages(String userId, String otherId) {
+    List<String> ids = [userId, otherId];
+    ids.sort();
+    String chatId = ids.join('_');
+
+    return _firestore.collection("chat_rooms").doc(chatId).collection("messages").orderBy("timesStamp",descending: false).snapshots();
   }
 }
